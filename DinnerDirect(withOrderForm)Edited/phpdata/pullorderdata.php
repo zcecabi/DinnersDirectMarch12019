d8349f963eac8cb771e2af489a96cb1d654c0960
carlacodes <carla.griffiths@gmail.com>
2019-02-28 16:36:17 +0000

    436PM 28_02_2019

--- DinnerDirect(withOrderForm)Edited/phpdata/pullorderdata.php
+++ DinnerDirect(withOrderForm)Edited/phpdata/pullorderdata.php
@@ -15,7 +15,7 @@ echo $userIDpullorderdatainstance;
 $query = "SELECT * FROM orders 
 FROM orders ord
 INNER JOIN customer cus /*alias of cus for customer*/
-    on ord.customer_id = cus.customer_id
+    on ord.customerID = cus.customerID
 WHERE customerID = '" . $userIDpullorderdatainstance."'";
 
 
